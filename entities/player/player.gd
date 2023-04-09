d0bfc728761f6cfe2074afcec056625010a5d6ab
ZeeHatcher <zee.hatcher010@gmail.com>
2023-04-09 17:28:18 +0800

    add camera, get global mouse position instead

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -44,7 +44,7 @@ func _handle_movement():
 
 
 func _face():
-	var mouse_position = get_viewport().get_mouse_position()
+	var mouse_position = get_global_mouse_position()
 	var direction = global_position.direction_to(mouse_position)
 	
 	_sprite.rotation = direction.angle() + PI / 2
