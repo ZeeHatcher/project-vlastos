20a9c023ef88b3e4eed12307d239409ae64096b1
ZeeHatcher <zee.hatcher010@gmail.com>
2023-04-09 17:21:49 +0800

    slow player movement when going against facing direction

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,18 +1,20 @@
 extends KinematicBody2D
 
 
-export (int) var speed = 200
+export(int) var speed = 200
+export(float) var slowness_multiplier = 0.5
 
 var _velocity = Vector2()
+var _facing = Vector2.UP
 
 onready var _sprite = $AnimatedSprite
 
 
 func _physics_process(delta):
 	_get_input()
-	_handle_animation()
 	_face()
-	_velocity = move_and_slide(_velocity)
+	_handle_movement()
+	_handle_animation()
 
 
 func _get_input():
@@ -25,7 +27,6 @@ func _get_input():
 		_velocity.y += 1
 	if Input.is_action_pressed("up"):
 		_velocity.y -= 1
-	_velocity = _velocity.normalized() * speed
 
 
 func _handle_animation():
@@ -33,8 +34,17 @@ func _handle_animation():
 	_sprite.playing = is_moving
 
 
+func _handle_movement():
+	var dot = _velocity.normalized().dot(_facing)
+	var sp = speed if dot > 0 else speed * slowness_multiplier
+	
+	_velocity = _velocity.normalized() * sp
+	_velocity = move_and_slide(_velocity)
+
+
 func _face():
 	var mouse_position = get_viewport().get_mouse_position()
 	var direction = global_position.direction_to(mouse_position)
 	
 	_sprite.rotation = direction.angle() + PI / 2
+	_facing = direction
