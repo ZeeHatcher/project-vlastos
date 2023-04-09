f5d453c29440c4552f109ca635c235e0acafa6b6
ZeeHatcher <zee.hatcher010@gmail.com>
2023-04-09 18:09:54 +0800

    send raycast when shoot

--- entities/gun/gun.gd
+++ entities/gun/gun.gd
@@ -7,13 +7,19 @@ export(int) var bullets = 3
 var _can_shoot = true
 
 onready var _timer = $CooldownTimer
+onready var _ray = $RayCast2D
 
 
 func shoot():
 	if bullets > 0 and _can_shoot:
-		bullets -= 1
 		print("shoot")
+		
+		var collider = _ray.get_collider()
+		if collider and collider.has_method("die"):
+			collider.die()
+		
 		emit_signal("shot")
+		bullets -= 1
 		_can_shoot = false
 		_timer.start()
 
